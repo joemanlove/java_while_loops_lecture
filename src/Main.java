336884306a64dc3eeeb666dcf13916c24555fbce
Joe Manlove <33290590+joemanlove@users.noreply.github.com>
2025-09-01 16:51:24 -0700

    added lecture content

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,32 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("While Loops!");
+
+        Scanner userInput = new Scanner(System.in);
+        String userInputString = "";
+
+        // while loop
+        /*
+        while (!userInputString.equalsIgnoreCase("yes") && !userInputString.equalsIgnoreCase("no")) {
+            System.out.println("Are you a meat popsicle?");
+            userInputString = userInput.nextLine();
+        }
+         */
+
+        // do while
+        // the do block always runs at least once, a while block may never run.
+        do {
+            System.out.println("Are you a meat popsicle?");
+            userInputString = userInput.nextLine();
+        } while (!userInputString.equalsIgnoreCase("yes") && !userInputString.equalsIgnoreCase("no"));
+
+        if (userInputString.equalsIgnoreCase("yes")){
+            System.out.println("Thanks for your affirmative response.");
+        } else {
+            System.out.println("Thanks for your negative response.");
+        }
+
     }
 }
